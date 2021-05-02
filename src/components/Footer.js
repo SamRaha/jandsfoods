b7c2f2b6fc16e9f72fa62e358351528defbf688b
Sam Raha <sam.raha@hermesinnovationlab.com>
2021-05-02 22:12:19 +0100

    need to sort out width

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,17 @@ import "./Footer.scss";
 function Footer() {
     return (
         <div className="footer-container">
-            <h1> Contact Us</h1>
-            <p>
-                <a href="tel:01132788266">0113 278 8266</a>
-            </p>
-            <p>Unit C3, Wyther Lane Industrial Estate, Leeds, LS5 3AP</p>
+            <div className="content">
+                <h1> Contact Us</h1>
+                <p>
+                    <a href="tel:01132788266">0113 278 8266</a>
+                </p>
+                <p>Unit C3, Wyther Lane Industrial Estate, Leeds, LS5 3AP</p>
 
-            <p>
-                <a href="mailto:info@jandsfoods.net">info@jandsfoods.net</a>
-            </p>
+                <p>
+                    <a href="mailto:info@jandsfoods.net">info@jandsfoods.net</a>
+                </p>
+            </div>
         </div>
     );
 }
