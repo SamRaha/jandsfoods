cd5beae54358263bc1893f8c04ca76bfb0d10cb6
Sam Raha <sam.raha@hotmail.co.uk>
2021-06-17 16:33:02 +0100

    more css changes

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -21,14 +21,18 @@ function Products() {
                         <div className="products-text-block">
                             <h3>Restaurants</h3>
                             <p>We have worked closely with small restaurants to multi-site restaurant groups throughout the years. We stock a range of quality products that have been sourced to meet our varying customer’s needs and new trends.</p>
-                            <a href="">View Menu</a>
+                            <a href="/Restaurant_Product_List.pdf" download>
+                                View Menu
+                            </a>
                         </div>
                     </div>
                     <div className="products-block">
                         <div className="products-text-block">
                             <h3>Schools</h3>
                             <p>Working closely with an array of primary, secondary and further education establishments has resulted in us stocking a range of school compliant products alongside the core products schools frequently use.</p>
-                            <a href="">View Menu</a>
+                            <a href="/School_Product_List.pdf" download>
+                                View Menu
+                            </a>
                         </div>
                         <div className="products-image-block schools"></div>
                     </div>
@@ -40,7 +44,9 @@ function Products() {
                                 Cafes have been J and S’s bread and butter since day one. We are proud to stock some of the best wholesale food goods to meet your café’s needs. Working closely with a diverse range of cafes, we can guarantee we stock
                                 what you require.
                             </p>
-                            <a href="">View Menu</a>
+                            <a href="/Cafe_Product_List.pdf" download>
+                                View Menu
+                            </a>
                         </div>
                     </div>
                     <div className="products-block">
@@ -50,7 +56,9 @@ function Products() {
                                 We pride ourselves on sourcing food goods that many other suppliers cannot provide. Our close working relationships with our suppliers has enabled us to provide an extensive range of vegan and plant based foods.
                                 Speciality dietary food products are available from gluten free, lactose free, nut free for example to assist with menu choses.
                             </p>
-                            <a href="">View Menu</a>
+                            <a href="/Vegan_Speciality_Product_List.pdf" download>
+                                View Menu
+                            </a>
                         </div>
                         <div className="products-image-block vegan"></div>
                     </div>
@@ -62,7 +70,9 @@ function Products() {
                                 We have been supplying mobile caterers for decades now, from vendors at The Leeds Festival and The Yorkshire Show to food vans at various sporting and music events or speciality vendors in The Trinity Kitchen. Rest
                                 assured we can work closely with you to provide food items to meet your businesses needs.
                             </p>
-                            <a href="">View Menu</a>
+                            <a href="/Festival_Street_Food_Product_List.pdf" download>
+                                View Menu
+                            </a>
                         </div>
                     </div>
                 </div>
